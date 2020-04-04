c214076038704a766d1d26791d2e1f714a1566f3
Vignav Ramesh <33817654+rvignav@users.noreply.github.com>
2020-04-04 16:45:07 -0700

    Add malaria prediction algorithm

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,6 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
 from flask import *
 from datetime import *
 from pytz import *
@@ -15,6 +18,7 @@ import os
 app = Flask(__name__)
 
 from keras.models import load_model
+from tensorflow.keras.models import load_model as load
 import cv2
 import numpy as np
 import h5py
@@ -27,6 +31,10 @@ import h5py
 @app.route('/upload')
 def upload():
     return render_template("index2.html")
+
+@app.route('/upload-malaria')
+def uploadMalaria():
+    return render_template("index3.html")
 
 diagnoses = []
 
@@ -50,11 +58,30 @@ def success():
             if (Class == 0):
                 today = str(get_pst_time())
                 diagnoses.append(today + ": According to our algorithm, it is likely that you have glaucoma. Please contact a medical professional as soon as possible for advice.")
-                return "It is likely that you have glaucoma. Please contact a medical professional as soon as possible for advice."
+                return f.filename + ": It is likely that you have glaucoma. Please contact a medical professional as soon as possible for advice."
             else:
                 today = str(get_pst_time())
                 diagnoses.append(today + ": According to our algorithm, you do not have glaucoma! If you have further questions, please contact a medical professional.")
-                return "According to our algorithm, you do not have glaucoma! If you have further questions, please contact a medical professional."
+                return f.filename + ": According to our algorithm, you do not have glaucoma! If you have further questions, please contact a medical professional."
+
+@app.route('/malaria', methods = ['POST'])
+def successMalaria():
+    if request.method == 'POST':
+        f2 = request.files['file']
+        f2.save(f2.filename)
+        h5file2 =  "/home/topdoc/mysite/weights.h5"
+        with h5py.File(h5file2,'r') as fid:
+            model2 = load(fid)
+            Class = prediction2(model2, f2.filename)
+            diagnoses.clear()
+            if (Class <= 0.5):
+                today = str(get_pst_time())
+                diagnoses.append(today + ": According to our algorithm, it is likely that you have malaria. Please contact a medical professional as soon as possible for advice.")
+                return f2.filename + ": It is likely that you have malaria. Please contact a medical professional as soon as possible for advice."
+            else:
+                today = str(get_pst_time())
+                diagnoses.append(today + ": According to our algorithm, you do not have malaria! If you have further questions, please contact a medical professional.")
+                return f2.filename + ": According to our algorithm, you do not have malaria! If you have further questions, please contact a medical professional."
 
 def autoroi(img):
 
@@ -73,16 +100,28 @@ def autoroi(img):
 
     return roi
 
+def prediction2(m, file):
+    # list_of_files = glob.glob('data/test/*')
+    # latest_file = max(list_of_files, key=os.path.getctime)
+    img = cv2.imread(file)
+    img = autoroi(img)
+    img = cv2.resize(img, (125, 125))
+    img = np.reshape(img, [1, 125, 125, 3])
+
+    Class = m.predict(img)
+    # Class = prob.argmax(axis=-1)
+
+    return(Class)
 
 def prediction(m, file):
     # list_of_files = glob.glob('data/test/*')
     # latest_file = max(list_of_files, key=os.path.getctime)
     img = cv2.imread(file)
     img = autoroi(img)
-    img = cv2.resize(img, (256, 256))
-    img = np.reshape(img, [1, 256, 256, 3])
+    img = cv2.resize(img, (125, 125))
+    img = np.reshape(img, [1, 125, 125, 3])
+    img = tf.cast(img, tf.float64)
 
-    prob = m.predict(img)
-    Class = prob.argmax(axis=-1)
+    Class = m.predict(img)
 
     return(Class)
